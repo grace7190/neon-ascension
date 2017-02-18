4914f5598acc527a3a54f85abe3746984ce1f97f
Daniel Son <danielsonjh@hotmail.com>
2017-02-18 13:31:16 -0500

    Added teams, death, and respawning.

---
+++ Assets/Players/PlayerDeathController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerDeathController : MonoBehaviour
+{
+    private PlayerController _playerController;
+
+    void Start()
+    {
+        _playerController = GetComponent<PlayerController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Tags.Block))
+        {
+            var collidingObject = other.gameObject;
+            var isUnderBlock = gameObject.transform.position.x - collidingObject.transform.position.x < 0.5
+                               && gameObject.transform.position.y < collidingObject.transform.position.y;
+            if (isUnderBlock && !_playerController.IsFalling)
+            {
+                TeamLivesManager.Instance.HandlePlayerDeath(gameObject);
+            }
+        }
+        else if (other.CompareTag(Tags.Destructor))
+        {
+            TeamLivesManager.Instance.HandlePlayerDeath(gameObject);
+        }
+    }
+}
---
+++ Assets/Teams/Teams.cs
@@ -0,0 +1,4 @@
+﻿public enum Teams
+{
+    Blue, Purple
+}
