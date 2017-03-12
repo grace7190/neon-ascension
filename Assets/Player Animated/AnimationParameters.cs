9be6d2a6f1b3a37cc852c1de90b4506f45f46de6
Jeffrey Deng <jeffreydeng@live.com>
2017-03-12 16:23:45 -0400

    Added WIP jumping animation, changed jumping
    - Changed Jumping from velocity, to Impulse force
    - Removed Gravity Multiplier on player

--- Assets/Player Animated/AnimationParameters.cs
+++ Assets/Player Animated/AnimationParameters.cs
@@ -1,6 +1,19 @@
 ﻿
 public class AnimationParameters {
-    public const string isWalking = "isWalking";
-    public const string isJumping = "isJumping";
-    public const string isJumpingMidair = "isJumpMidair";
+    public const string IsWalking = "isWalking";
+    public const string IsJumping = "isJumping";
+    public const string IsJumpingMidair = "isJumpMidair";
+
+    //public const string 
+    // Animation names
+    public const string PullingName = "Pulling_NoRoot";
+    public const string PushingName = "Pushing_NoRoot";
+    public const string WalkingName = "Walking_NoRoot";
+    public const string JumpLandingName = "Jump_Landing_NoRoot";
+    public const string JumpMidairName = "Jump_Midair_NoRoot";
+    public const string JumpTakeoffName = "Jump_Takeoff_NoRoot";
+    public const string ClimbingName = "Climbing_NoRoot";
+    public const string FallingFarName = "Falling_Far_NoRoot";
+    public const string IdleName = "Idle_NoRoot";
+    public const string CrushedName = "Crushed_NoRoot";
 }
