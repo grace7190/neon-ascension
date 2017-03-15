67c506ad6a3b0d4e6a5525988aade7cd5c2e7e99
Daniel Son <danielsonjh@hotmail.com>
2017-03-14 22:17:21 -0400

    Unnuked keyboard input.

--- Assets/Players/PlayerDeathController.cs
+++ Assets/Players/PlayerDeathController.cs
@@ -2,13 +2,6 @@
 
 public class PlayerDeathController : MonoBehaviour
 {
-    private PlayerController _playerController;
-
-    void Start()
-    {
-        _playerController = GetComponent<PlayerController>();
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(Tags.Block))
