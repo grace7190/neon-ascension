e2e8a548c784a420bfae13cc194537aa12fef7df
grace7190 <grace.zhu@mail.utoronto.ca>
2017-03-16 06:48:26 -0400

    forgot a thing

---
+++ Assets/Players/PlayerFacingBlockDetector.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFacingBlockDetector : MonoBehaviour {
+
+    public GameObject HighlightedObject = null;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.Block && other.gameObject.layer == Layers.Solid)
+        {
+            var script = other.GetComponent<Block> ();
+            if (gameObject.name == "Player1Child")
+            {
+                script.ChangeColor (Color.yellow, Block.ChangeColorDuration);
+            }
+            else
+            {
+                script.ChangeColor (Color.green, Block.ChangeColorDuration);
+            }
+            HighlightedObject = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == Tags.Block) {
+            var script = other.GetComponent<Block> ();
+            script.ChangeColor (script.BaseColor, Block.ChangeColorDuration);
+
+            if (HighlightedObject == other.gameObject)
+            {
+                HighlightedObject = null;
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        if (HighlightedObject != null)
+        {
+            var block = HighlightedObject.GetComponent<Block>();
+            block.ChangeColor (block.BaseColor, Block.ChangeColorDuration);
+        }
+    }
+}
