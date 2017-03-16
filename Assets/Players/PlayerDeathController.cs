1c71f60d7ed190f6717267cc77ad6ac2c2d77510
Jeffrey Deng <jeffreydeng@live.com>
2017-03-16 00:27:59 -0400

    Fixed bugs
    -- Moved death collider to child, and also moved the DeathController to child, to prevent colliders with triggers from being represented as a whole
    -- Removed float added in DeathController because of new trigger for block highlights
    -- Removed z index lock on players
    -- Fix player inversion for player 2

--- Assets/Players/PlayerDeathController.cs
+++ Assets/Players/PlayerDeathController.cs
@@ -8,17 +8,16 @@ public class PlayerDeathController : MonoBehaviour
         {
             var collidingObject = other.gameObject;
             var isUnderBlock = gameObject.transform.position.x - collidingObject.transform.position.x < 0.5
-                               && gameObject.transform.position.y + 0.5 < collidingObject.transform.position.y  
+                               && gameObject.transform.position.y < collidingObject.transform.position.y
                                && other.GetComponent<Rigidbody>().velocity.y < 0;
             if (isUnderBlock)
             {
-				Debug.Log (collidingObject.transform.position.y - gameObject.transform.position.y);
-                TeamLivesManager.Instance.HandlePlayerDeath(gameObject);
+                TeamLivesManager.Instance.HandlePlayerDeath(gameObject.transform.parent.gameObject);
             }
         }
         else if (other.CompareTag(Tags.Destructor))
         {
-            TeamLivesManager.Instance.HandlePlayerDeath(gameObject);
+            TeamLivesManager.Instance.HandlePlayerDeath(gameObject.transform.parent.gameObject);
         }
     }
 }
