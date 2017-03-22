da2c331c612c1d6d2c6b3def9aa844b1b97fb971
Catherine Wang <yijin.wang@mail.utoronto.ca>
2017-03-22 11:35:57 -0400

    Flip Camera 2 and XRay 2 on Player 2

---
+++ Assets/Camera/MirrorXRay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Reference: http://wiki.unity3d.com/index.php?title=InvertCamera
+public class MirrorXRay : MonoBehaviour {
+
+	private Camera cam;
+
+	void Start() {
+		cam = GetComponent<Camera> ();
+	}
+
+	void OnPreCull () {
+		cam.ResetWorldToCameraMatrix ();
+		cam.ResetProjectionMatrix ();
+		Vector3 v = new Vector3 (-1, 1, 1);
+		Matrix4x4 mat= Matrix4x4.Scale (v);
+		cam.projectionMatrix = cam.projectionMatrix * mat;
+	}
+
+	void OnPreRender () {
+		GL.invertCulling = true;
+	}
+
+	void OnPostRender () {
+		GL.invertCulling = false;
+	}
+}
