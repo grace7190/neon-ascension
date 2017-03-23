e23a3557c15afc31e47130246c2e4589676f90c0
g3bilode <philip.bilodeau@gmail.com>
2017-03-22 23:58:42 -0400

    Shortened tutorial, appears on game restart

--- Assets/HUD/TutorialManager.cs
+++ Assets/HUD/TutorialManager.cs
@@ -10,19 +10,13 @@ public class TutorialManager : MonoBehaviour {
 
     public GameObject BlueTutorial1;
     public GameObject PurpleTutorial1;
-    public GameObject BlueTutorial2;
-    public GameObject PurpleTutorial2;
-    public GameObject BlueTutorial3a;
-    public GameObject PurpleTutorial3a;
-    public GameObject BlueTutorial3b;
-    public GameObject PurpleTutorial3b;
     public GameObject Tutorial4;
 
     private bool _onceToken;
 
     //replace with actual steps later
     private static readonly List<float> tutorialTimes = new List<float> {
-        4.0f, 12.0f, 14.0f, 18.0f, 20.0f, 25.0f, 27.0f, 28.5f, 29.0f};
+        3.0f, 11.0f, 12.0f, 15.0f, 17.0f, 18.0f };
 
     // Use this for initialization
     void Start () {
@@ -30,21 +24,15 @@ public class TutorialManager : MonoBehaviour {
 
         BlueTutorial1.GetComponent<Image>().canvasRenderer.SetAlpha(0.0f);
         PurpleTutorial1.GetComponent<Image>().canvasRenderer.SetAlpha(0.0f);
-        BlueTutorial2.GetComponent<Image>().canvasRenderer.SetAlpha(0.0f);
-        PurpleTutorial2.GetComponent<Image>().canvasRenderer.SetAlpha(0.0f);
-        BlueTutorial3a.GetComponent<Image>().canvasRenderer.SetAlpha(0.0f);
-        PurpleTutorial3a.GetComponent<Image>().canvasRenderer.SetAlpha(0.0f);
-        BlueTutorial3b.GetComponent<Image>().canvasRenderer.SetAlpha(0.0f);
-        PurpleTutorial3b.GetComponent<Image>().canvasRenderer.SetAlpha(0.0f);
         Tutorial4.GetComponent<Image>().canvasRenderer.SetAlpha(0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {   
-        if (TutorialStep < getCurrentStep(Time.time))
+        if (TutorialStep < getCurrentStep(Time.timeSinceLevelLoad))
         {
-            TutorialStep = getCurrentStep(Time.time);
+            TutorialStep = getCurrentStep(Time.timeSinceLevelLoad);
             ProgressTutorial();
         }
 
@@ -75,45 +63,22 @@ public class TutorialManager : MonoBehaviour {
             case 2:
                 BlueTutorial1.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.0f, false);
                 PurpleTutorial1.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.0f, false);
-                break;
-            case 3:
-                BlueTutorial2.GetComponent<Image>().CrossFadeAlpha(1.0f, 0.5f, false);
-                PurpleTutorial2.GetComponent<Image>().CrossFadeAlpha(1.0f, 0.5f, false);
-                break;
-            case 4:
-                BlueTutorial2.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.0f, false);
-                PurpleTutorial2.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.0f, false);
                 BlockColumnManager.Instance.CameraScrollEnabled = true;
                 Debug.Log("scrolling enabled");
                 break;
-            case 5:
-                BlueTutorial3a.GetComponent<Image>().CrossFadeAlpha(1.0f, 0.5f, false);
-                PurpleTutorial3a.GetComponent<Image>().CrossFadeAlpha(1.0f, 0.5f, false);
-                break;
-            case 6:
-                BlueTutorial3a.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.0f, false);
-                PurpleTutorial3a.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.0f, false);
-                Debug.Log("falling blocks enabled");
-                BlockColumnManager.Instance.BlockRainEnabled = true;
-                break;
-            case 7:
+            case 3:
                 Tutorial4.GetComponent<Image>().CrossFadeAlpha(1.0f, 0.5f, false);
                 break;
-            case 8:
+            case 4:
                 Tutorial4.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.0f, false);
-                TeamLivesManager.Instance.DeathEnabled = true;
+                Debug.Log("falling blocks enabled");
+                BlockColumnManager.Instance.BlockRainEnabled = true;
                 Debug.Log("death enabled");
+                TeamLivesManager.Instance.DeathEnabled = true;
                 break;
-            case 9:
+            case 5:
                 Destroy(BlueTutorial1);
                 Destroy(PurpleTutorial1);
-                Destroy(BlueTutorial2);
-                Destroy(PurpleTutorial2);
-                Destroy(BlueTutorial1);
-                Destroy(BlueTutorial3a);
-                Destroy(PurpleTutorial3a);
-                Destroy(BlueTutorial3b);
-                Destroy(PurpleTutorial3b);
                 Destroy(Tutorial4);
                 break;
             default:
