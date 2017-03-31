ea3ac1f3e200abdc409d68fa067873d6c707ea22
grace7190 <grace.zhu@mail.utoronto.ca>
2017-03-30 23:50:49 -0400

    starting work

---
+++ Assets/HUD/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public GUIText blueScore;
+    public GUIText purpleScore;
+
+	// Use this for initialization
+	void Start () {
+     
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
