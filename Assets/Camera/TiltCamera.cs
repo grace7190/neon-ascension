7cc8f002a1c27d46f0b7ea0b48f9bb66c966a476
Catherine Wang <yijin.wang@mail.utoronto.ca>
2017-04-02 00:01:56 -0400

    Tilt gradually

--- Assets/Camera/TiltCamera.cs
+++ Assets/Camera/TiltCamera.cs
@@ -6,24 +6,33 @@ public class TiltCamera : MonoBehaviour {
 
 	Camera cam;
 	public GameObject player;
-
 	public float tiltMaxAngle;
+	public float speed;
+
 	private float degreePerPixel;
-	private float preScreenY;
+	private float initScreenY;
+	//private float preScreenY;
 	private float curScreenY;
+	private float initX;
 
 	// Use this for initialization
 	void Start () {
 		cam = GetComponent<Camera>();
-		preScreenY = cam.WorldToScreenPoint (player.transform.position).y;
-		degreePerPixel = tiltMaxAngle / (Screen.height - preScreenY);
+		initX = transform.rotation.eulerAngles.x;
+		initScreenY = cam.WorldToScreenPoint (player.transform.position).y;
+		degreePerPixel = tiltMaxAngle / (Screen.height - initScreenY);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		curScreenY = cam.WorldToScreenPoint (player.transform.position).y;
-		float rotateDegree = degreePerPixel * (preScreenY - curScreenY);
-		transform.Rotate (new Vector3 (rotateDegree, 0, 0));
-		preScreenY = curScreenY;
+		float step = speed * Time.deltaTime;
+		float curX = initX - degreePerPixel * (curScreenY - initScreenY);
+		transform.rotation = Quaternion.RotateTowards (
+			transform.rotation, 
+			Quaternion.Euler (curX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z), 
+			step);
+		//transform.Rotate (new Vector3 (rotateDegree, 0, 0));
+		//preScreenY = curScreenY;
 	}
 }
