42981e044a98ea8ea8d769dea03db1ef4e9dda7e
Jeffrey Deng <jeffreydeng@live.com>
2017-04-03 01:20:10 -0400

    Fix movement of camera, and adjusted rotation

--- Assets/Camera/TiltCamera.cs
+++ Assets/Camera/TiltCamera.cs
@@ -4,72 +4,69 @@ using UnityEngine;
 
 public class TiltCamera : MonoBehaviour {
 
-	public GameObject player;
-	public GameObject blockColumnManager;
-	public float maxAngleChange;			// Max degree of angle to change from initial angle
-	public float maxMovement;				// Max offset to move the camera for tilting
-	public int maxFrameUpdate;				// Number of frames used to update tilting to target.
-	public float minScreenBaseY;			// Min Y of base's location on screen
-	public float maxScreenBaseY;			// Max Y of base's location on screen
+    public GameObject player;
+    public GameObject blockColumnManager;
+    public float maxAngleChange;            // Max degree of angle to change from initial angle
+    public float maxMovement;               // Max offset to move the camera for tilting
+    public int maxFrameUpdate;              // Number of frames used to update tilting to target.
 
-	private Camera cam;
-	private BoxCollider supportBoxCollider;
-	private float maxDiff = 15.0f;			// maxDiff is set to the wall height
-	private float anglePerDiff;	
-	private float movePerDiff;
-	private float initAngle;
-	private float initDiff;
-	private float preOffset;				// Previous offset
-	private int numFrameUpdate;				// Count number of frames being used for update
-	private float anglePerFrame;			// How many degrees to tilt per frame
-	private float movePerFrame;				// The distance to move per frame for tilting
+    private BoxCollider supportBoxCollider;
+    private float maxDiff = 15.0f;          // maxDiff is set to the wall height
+    private float anglePerDiff; 
+    private float movePerDiff;
+    private float initAngle;
+    private float initDiff;
+    private float preOffset;                // Previous offset
+    private float anglePerFrame;            // How many degrees to tilt per frame
+    private float movePerFrame;             // The distance to move per frame for tilting
 
-	// Use this for initialization
-	void Start () {
-		cam = GetComponent<Camera> ();
-		supportBoxCollider = blockColumnManager.GetComponent<BoxCollider>();
-		initDiff = Mathf.Floor(player.transform.position.y - supportBoxCollider.center.y);
-		anglePerDiff = maxAngleChange / maxDiff;
-		movePerDiff = maxMovement / maxDiff;
-		initAngle = transform.rotation.eulerAngles.x;
-		preOffset = 0;
-		anglePerFrame = 0;
-		movePerFrame = 0;
-		numFrameUpdate = maxFrameUpdate;
-	}
+    // Use this for initialization
+    void Start () {
+        supportBoxCollider = blockColumnManager.GetComponent<BoxCollider>();
+        initDiff = Mathf.Floor(player.transform.position.y - supportBoxCollider.center.y);
+        anglePerDiff = maxAngleChange / maxDiff;
+        movePerDiff = maxMovement / maxDiff;
+        initAngle = transform.rotation.eulerAngles.x;
+        preOffset = 0;
+        anglePerFrame = 0;
+        movePerFrame = 0;
+    }
 
-	// Update is called once per frame
-	void Update () {
-		// Tilt camera only when player has at least 1 life.
-		if (player != null) {
-			// Recalculate target tilting angle and camera distance only when previous update is done.
-			if (numFrameUpdate >= maxFrameUpdate) {
-				float curDiff = Mathf.Floor (player.transform.position.y - supportBoxCollider.center.y);
-				float actDiff = curDiff - initDiff;
-				float curAngle = initAngle + anglePerDiff * actDiff;
-				float curOffset = movePerDiff * actDiff;
-				float offsetDiff = curOffset - preOffset;
-				anglePerFrame = Mathf.DeltaAngle(transform.rotation.eulerAngles.x, curAngle) / maxFrameUpdate;
-				movePerFrame = offsetDiff / maxFrameUpdate;
-				preOffset = curOffset;
-				numFrameUpdate = 0;
-			} else {
-				numFrameUpdate += 1;
-			}
-			transform.rotation = Quaternion.Euler (
-				transform.rotation.eulerAngles.x + anglePerFrame,
-				transform.rotation.eulerAngles.y,
-				transform.rotation.eulerAngles.z
-			);
-			// Don't update distance once base is outside of designed range on screen.
-			if (cam.WorldToScreenPoint(supportBoxCollider.center).y > minScreenBaseY 
-				&& cam.WorldToScreenPoint(supportBoxCollider.center).y < maxScreenBaseY){
-				transform.position = new Vector3 (
-					transform.position.x, 
-					transform.position.y + movePerFrame, 
-					transform.position.z
-				);
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update () {
+        // Tilt camera only when player has at least 1 life.
+        if (player != null && player.activeInHierarchy) {
+            float curDiff = Mathf.Floor (player.transform.position.y - supportBoxCollider.center.y);
+            float actDiff = curDiff - initDiff;
+            float curAngle = initAngle + anglePerDiff * actDiff;
+            float curOffset = movePerDiff * actDiff; // How much the camera should have been offset
+            float offsetDiff = curOffset - preOffset; // The Amount we need to move
+            
+            anglePerFrame = Mathf.DeltaAngle(transform.rotation.eulerAngles.x, curAngle) / maxFrameUpdate;
+            movePerFrame = offsetDiff / maxFrameUpdate; // The ammount we move per frame
+                     
+            transform.rotation = Quaternion.Euler (
+                transform.rotation.eulerAngles.x + anglePerFrame,
+                transform.rotation.eulerAngles.y,
+                transform.rotation.eulerAngles.z
+            );
+
+            // Don't let the Y movement go over MaxDiff or below 0
+            if (preOffset + movePerFrame > maxDiff)
+            {
+                movePerFrame = maxDiff - preOffset;
+            }
+            else if (preOffset + movePerFrame < 0)
+            {
+                movePerFrame = 0 - preOffset;
+            }
+
+            preOffset += movePerFrame;
+            transform.position = new Vector3 (
+                transform.position.x, 
+                transform.position.y + movePerFrame, 
+                transform.position.z
+            );
+        }
+    }
 }
